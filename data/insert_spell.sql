54c69e453626eac56f3d63ea7288e53ef3cb255f
Dreznel <JuunCaerlum@gmail.com>
2020-05-29 00:03:03 -0400

    wip: messing around with database stuff and therefore adding a simple script to inject some data.

---
+++ data/insert_spell.sql
@@ -0,0 +1,13 @@
+INSERT INTO spell (`name`, magic_type, fade_die, fade_on, minimum_cast, `range`, duration, requirements, effect)
+VALUES(
+'Bolt',
+'Water/Cold, Fire, Earth, Air, Lightning, Force, Acid, Light, Corruption, Mind',
+'d6',
+'1-2',
+1,
+'30 feet',
+'instant',
+'',
+'Make a ranged attack (using Wisdom) against a target within range. If the attack hits, deal 2d8 damage of your chosen magic type to that creature.
+The conjuring of the bolt is easy, it is the aiming that you are rolling for, which is why this spell uses Wisdom.'
+);
